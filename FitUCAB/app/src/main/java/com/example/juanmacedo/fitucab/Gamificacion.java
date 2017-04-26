3741dd7d67384941374263dbd172c460ce5aa0fd
JuanMacedo <juanmacedoal@gmail.com>
2017-04-25 21:44:07 -0400

    Fragment agregado onclick lista *

--- FitUCAB/app/src/main/java/com/example/juanmacedo/fitucab/Gamificacion.java
+++ FitUCAB/app/src/main/java/com/example/juanmacedo/fitucab/Gamificacion.java
@@ -1,13 +1,22 @@
 package com.example.juanmacedo.fitucab;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class Gamificacion extends AppCompatActivity {
+public class Gamificacion extends AppCompatActivity implements View.OnClickListener, Logros.OnFragmentInteractionListener{
 
     private ArrayList<String> arrayList;
     private String[] logros = {"5 Km recorrido", "10 km recorrido", "Regimen Agua", "Gana a un amigo", "Invita a un amigo"};
@@ -23,5 +32,50 @@ public class Gamificacion extends AppCompatActivity {
         arrayList = new ArrayList<String>();
         adapter = new ArrayAdapter<String>(this, R.layout.contenido,R.id.tv_logro, logros);
         lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView item = (TextView) view.findViewById(R.id.tv_logro);
+                Bundle bundle = new Bundle();
+                bundle.putString("item", (String) item.getText());
+                Log.d("DATOOO", (String) item.getText());
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                Logros fragment = new Logros();
+                fragment.setArguments(bundle);
+                transaction.add(R.id.gamificacion, fragment);
+
+                transaction.commit();
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        Logros fragment = new Logros();
+
+        transaction.add(R.id.gamificacion, fragment);
+
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ FitUCAB/app/src/main/java/com/example/juanmacedo/fitucab/ItemAdapter.java
@@ -0,0 +1,31 @@
+package com.example.juanmacedo.fitucab;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by juanmacedo on 25/04/17.
+ */
+
+public class ItemAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
