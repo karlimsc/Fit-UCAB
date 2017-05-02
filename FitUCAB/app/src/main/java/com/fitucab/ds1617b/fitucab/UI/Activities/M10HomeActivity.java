c44231dc3fd9722be376f515005bdf48a9c7cbcc
Charbel <charbelnacharn@gmail.com>
2017-05-02 01:52:59 -0400

    arregle el esqueleto , cree la carpeta de los fragments y la parte logica de la activity

---
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M10HomeActivity.java
@@ -0,0 +1,13 @@
+package com.fitucab.ds1617b.fitucab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_m10_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m10_home);
+    }
+}
