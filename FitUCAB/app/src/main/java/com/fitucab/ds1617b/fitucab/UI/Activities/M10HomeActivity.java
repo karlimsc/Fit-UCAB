32ccc9e225bfa1c1de43304e8226fcb91e708027
Charbel <charbelnacharn@gmail.com>
2017-05-08 13:05:51 -0400

    cree un cable para abrir nuestra vista  se abre desde el login

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M10HomeActivity.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M10HomeActivity.java
@@ -3,7 +3,7 @@ package com.fitucab.ds1617b.fitucab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity_m10_home extends AppCompatActivity {
+public class M10HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
