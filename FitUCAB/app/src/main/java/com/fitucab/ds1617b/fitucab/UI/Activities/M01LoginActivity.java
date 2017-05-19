34c3227144d4ac47cd011c62e2b63cb278b5f5d5
Charbel <charbelnacharn@gmail.com>
2017-05-19 12:04:43 -0400

    arregle todo y ya puse el cable

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M01LoginActivity.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M01LoginActivity.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 package com.fitucab.ds1617b.fitucab.UI.Activities;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,107 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-
-import com.fitucab.ds1617b.fitucab.Model.Helper.OnFragmentSwap;
-import com.fitucab.ds1617b.fitucab.R;
-import com.fitucab.ds1617b.fitucab.UI.Fragments.M01.M01HomeFragment;
-import com.fitucab.ds1617b.fitucab.UI.Fragments.M01.M01LoginFragment;
-import com.fitucab.ds1617b.fitucab.UI.Fragments.M01.M01RecoveryFragment;
-import com.fitucab.ds1617b.fitucab.UI.Fragments.M01.M01SignUpFragment;
-
-import java.util.List;
-
-public class M01LoginActivity extends AppCompatActivity implements OnFragmentSwap {
-
-    private FragmentManager FM = getSupportFragmentManager();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        //Declaracion del Toolbar. si alguien necesita acceder al toolbar en un Fragment
-        //debe declarar este atributo como privado y hacerle un Getter.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        //Seleccionamos el fragmento que queremos mostrar.
-        onSwap("M01HomeFragment",null);
-    }
-
-    //cambiar fragment
-    @Override
-    public void onSwap(String fragmentName, Bundle bundle) {
-        //Declaramos un Fragmento nulo (al que vamos a cambiar)
-
-        Fragment fragmentToSwap = null;
-        FragmentTransaction fragmentTransaction = FM.beginTransaction();
-
-        try {
-            switch (fragmentName) {
-                case "M01HomeFragment":
-                    fragmentToSwap = new M01HomeFragment();
-                    fragmentToSwap.setArguments(bundle);
-                    fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
-                    break;
-
-                case "M01SignUpFragment":
-                    fragmentToSwap = new M01SignUpFragment();
-                    fragmentToSwap.setArguments(bundle);
-                    fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
-                    fragmentTransaction.addToBackStack("M01SignUpFragment");
-
-                    break;
-
-                case "M01LoginFragment":
-                    fragmentToSwap = new M01LoginFragment();
-                    fragmentToSwap.setArguments(bundle);
-                    fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
-                    fragmentTransaction.addToBackStack("M01LoginFragment");
-
-                    break;
-                case "M01RecoveryFragment":
-                    fragmentToSwap = new M01RecoveryFragment();
-                    fragmentToSwap.setArguments(bundle);
-                    fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
-                    fragmentTransaction.addToBackStack("M01RecoveryFragment");
-
-                    break;
-            }
-            fragmentTransaction.commit();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onSwapActivity(String activityName, Bundle bundle) {
-
-        switch (activityName){
-            case "M02HomeActivity":
-                /*pequeño cable*/
-                Intent newActivity = new Intent(this, M10WaterGlassActivity.class);
-                startActivity(newActivity);
-                break;
-
-        }
-
-    }
-
-}
-=======
-package com.fitucab.ds1617b.fitucab.UI.Activities;
-import android.content.Intent;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.method.PasswordTransformationMethod;
-import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.fitucab.ds1617b.fitucab.Model.Helper.OnFragmentSwap;
 import com.fitucab.ds1617b.fitucab.R;
@@ -200,7 +97,7 @@ public class M01LoginActivity extends AppCompatActivity implements OnFragmentSwa
 
         switch (activityName){
             case "M02HomeActivity":
-                Intent newActivity = new Intent(this, M02HomeActivity.class);
+                Intent newActivity = new Intent(this, M10WaterGlassActivity.class);
                 startActivity(newActivity);
                 break;
 
@@ -209,4 +106,3 @@ public class M01LoginActivity extends AppCompatActivity implements OnFragmentSwa
     }
 
 }
->>>>>>> Develop
--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M05/M05TrackActivityFragment.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M05/M05TrackActivityFragment.java
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-package com.fitucab.ds1617b.fitucab.UI.Fragments.M05;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.fitucab.ds1617b.fitucab.Model.Helper.OnFragmentSwap;
-import com.fitucab.ds1617b.fitucab.R;
-
-/**
- * Created by karo on 13/04/17.
- */
-
-public class M05TrackActivityFragment extends Fragment {
-
-    private OnFragmentSwap _callBack;
-
-    public M05TrackActivityFragment (){
-        // Required empty public constructor
-    }
-
-
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Log.i("TRACKFRAGMENTCLASS","OnCreateView");
-        View _view = inflater.inflate(R.layout.fragment_m05_track_activity, container, false);
-        //TextView _textView = (TextView) _view.findViewById(R.id.trackText);
-
-        return _view;
-    }
-}
-=======
 package com.fitucab.ds1617b.fitucab.UI.Fragments.M05;
 
 import android.os.Bundle;
@@ -109,4 +68,3 @@ public class M05TrackActivityFragment extends Fragment implements OnMapReadyCall
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
->>>>>>> Develop
