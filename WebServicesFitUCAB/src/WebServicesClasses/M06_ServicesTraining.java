788cb973fcf9d7a91fc84d708ed74185b328aa52
Spolm <luismachadoz.12@gmail.com>
2017-05-20 15:20:21 -0400

    Agregados archivos del WebService
    Se agregaron los archivos Training.java (domain) M06_ServicesTraining (clase del webservice) y se modifico FitUCAB.java para agregar la llamada al M06 en el hash

---
+++ WebServicesFitUCAB/src/WebServicesClasses/M06_ServicesTraining.java
@@ -0,0 +1,98 @@
+package WebServicesClasses;
+
+import Domain.Training;
+
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.sql.*;
+
+/**
+ * Created by LuisMiguel on 18/05/2017.
+ */
+@Path("/training")
+public class M06_ServicesTraining {
+
+    Gson gson = new Gson();
+
+    //el tipo de instruccion HTTP
+    @GET
+    //el path de la funcion
+    @Path("/createTraining")
+    //formato de retorno
+    @Produces("application/json")
+
+    /***
+     * Metodo utilizado a traves de web service para agregar los parametros a la base de datos
+     * @param trainingName
+     * @param trainingPeriod
+     * @param trainingCalories
+     */
+    public String createTraining(@QueryParam("trainingName") String name, @QueryParam("id") int id,
+                                 @QueryParam("trainingPeriod") int period, @QueryParam("trainingCalories") int calories) {
+        String query = "INSERT INTO TRAINING (NAME, ID, PERIOD, CALORIES) VALUES (name,id,period,calories)";
+
+        try {
+            Connection conn = connectDb();
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+            return gson.toJson(true);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @GET
+    @Path("/displayTraining")
+    @Produces("application/json")
+    public String getTraining(@QueryParam("id") int id) {
+        String name = "";
+        int period = 0;
+        int calories = 0;
+        String query = "SELECT * FROM TRAINING";
+        Training results = new Training(id, name, period, calories);
+        try {
+            Connection conn = connectDb();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+
+                results.setId(rs.getInt("id"));
+                results.setTrainingName(rs.getString("trainingName"));
+                results.setTrainingPeriod(rs.getInt("trainingPeriod"));
+                results.setTrainingCalories(rs.getInt("trainingCalories"));
+
+            }
+            return gson.toJson(results);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * Metodo que sirve para crear el conector a la base de datos
+     *
+     * @return un conector para hacer llamadas a la BD
+     */
+    private Connection connectDb() {
+        Connection conn = null;
+        try {
+            //llamada al driver de PostgreSQL
+            Class.forName("org.postgresql.Driver");
+            //String de conexion a la db:
+            String url = "jdbc:postgresql:FitUCAB";
+
+            conn = DriverManager.getConnection(url, "postgres", "211289");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+        return conn;
+    }
+}
