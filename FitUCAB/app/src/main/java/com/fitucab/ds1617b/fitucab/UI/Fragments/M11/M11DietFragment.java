b83173a61aea20a8c290f3d917acda503ddc788f
zedin1994 <94gagb@gmail.com>
2017-05-22 19:42:18 -0400

    Fragmento diet, solo falta sw, la llamada y el eliminado de la lista

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M11/M11DietFragment.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M11/M11DietFragment.java
@@ -2,20 +2,26 @@ package com.fitucab.ds1617b.fitucab.UI.Fragments.M11;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.fitucab.ds1617b.fitucab.Helper.OnFragmentSwap;
 import com.fitucab.ds1617b.fitucab.R;
 import android.widget.ArrayAdapter;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,12 +31,13 @@ public class M11DietFragment extends Fragment {
 
 
     private TextView _tv_m11_cantidadCalorias;
-    private TextView _tv_m11_nombreAlimento;
-    private TextView _tv_m11_peso;
+    private AutoCompleteTextView _tv_m11_nombreAlimento;
+    private EditText _tv_m11_peso;
     private ImageButton _btn_m11_agregar;
     private Spinner _spinner_comida;
     private ImageButton _btn_m11_aceptar;
     private ImageButton _btn_m11_rechazar;
+    private ImageButton _btn_m11__diet_borrar;
     private TableLayout _tl_m11_listaDieta;
     private View _view;
     private OnFragmentSwap _callBack;
@@ -68,16 +75,81 @@ public class M11DietFragment extends Fragment {
 
 
         _tv_m11_cantidadCalorias = (TextView) _view.findViewById(R.id.tv_m11_cantidadCalorias);
-        _tv_m11_nombreAlimento = (TextView) _view.findViewById(R.id.tv_m11_nombreAlimento);
-        _tv_m11_peso = (TextView) _view.findViewById(R.id.tv_m11_peso);
+        _tv_m11_nombreAlimento = (AutoCompleteTextView) _view.findViewById(R.id.tv_m11_nombreAlimento);
+        _tv_m11_peso = (EditText) _view.findViewById(R.id.tv_m11_peso);
         _btn_m11_agregar = (ImageButton) _view.findViewById(R.id.btn_m11_agregar);
         _spinner_comida = (Spinner) _view.findViewById(R.id.spinner_comida);
         _btn_m11_aceptar = (ImageButton) _view.findViewById(R.id.btn_m11_aceptar);
         _btn_m11_rechazar = (ImageButton) _view.findViewById(R.id.btn_m11_rechazar);
         _tl_m11_listaDieta = (TableLayout) _view.findViewById(R.id.tl_m11_listaDieta);
-
-
-
+        _btn_m11__diet_borrar = (ImageButton) _view.findViewById(R.id.btn_m11_diet_borrar);
+        setAlimento();
+        AddListenerBack();
         return _view;
+    }
+
+    /**
+     * Metodo que inicializa el listener para agregar alimentos a la dieta.
+     */
+    public void setAlimento(){
+        _btn_m11_agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombreAlimento =  _tv_m11_nombreAlimento.getText().toString();
+                String pesoAlimento = _tv_m11_peso.getText().toString();
+                LlenaTabla(nombreAlimento, pesoAlimento);
+               // LlenaTabla("basura"); -> En caso de que usemos el icono para borrar de la lista
+            }
+        });
+    }
+
+    /**
+     * Metodo que inicializa el listener para volver atras y cancelar la carga de la dieta.
+     */
+    public void AddListenerBack()
+    {
+        _btn_m11_rechazar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _callBack.onSwap("M11FoodhomeActivity",null);
+            }
+        });
+    }
+
+    /**
+     * Metodo que realiza la carga de el alimento a la lista de la dieta
+     * @param nombreAlimento Recibe nombre del alimento a cargar,
+     * @param pesoAlimento Recibe el peso del alimento a cargar.
+     */
+    public void LlenaTabla( String nombreAlimento, String pesoAlimento )
+    {
+        /* Esto solo si decidimos usar el simbolo para borrar de la lista.
+        if ( nombreAlimento.equals( "basura" ) && ( pesoAlimento.equals( "basura" ) ))
+        {
+            TableRow tableRow = new TableRow(getContext());
+            TableLayout.LayoutParams params = new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.WRAP_CONTENT , TableLayout.LayoutParams.WRAP_CONTENT);
+            ImageButton currentImage = new ImageButton(getContext());
+            currentImage = _btn_m11__diet_borrar;
+            currentImage.setVisibility(View.VISIBLE);
+            tableRow.setLayoutParams(params);
+            tableRow.addView(currentImage);
+
+            _tl_m11_listaDieta.addView(tableRow);
+        }
+        else
+        {*/
+            TableRow tableRow = new TableRow(getContext());
+            TableLayout.LayoutParams params = new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.WRAP_CONTENT , TableLayout.LayoutParams.WRAP_CONTENT);
+            TextView currentText = new TextView(getContext());
+            currentText.setText(nombreAlimento+"             "+pesoAlimento);
+            currentText.setTextSize(currentText.getText().length());
+            currentText.setTextColor(Color.BLACK);
+            tableRow.setLayoutParams(params);
+            tableRow.addView(currentText);
+            _tl_m11_listaDieta.addView(tableRow);
+        //}
+
     }
 }
