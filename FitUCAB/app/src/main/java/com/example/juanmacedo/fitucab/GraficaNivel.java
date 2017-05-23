1ef4cad278890157b7788665273ccebff67674c8
bdc884 <bdc884@gmail.com>
2017-05-22 23:16:45 -0400

    borrando imports en deshuso

--- FitUCAB/app/src/main/java/com/example/juanmacedo/fitucab/GraficaNivel.java
+++ FitUCAB/app/src/main/java/com/example/juanmacedo/fitucab/GraficaNivel.java
@@ -4,14 +4,13 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.MPPointF;
+
 
 import java.util.ArrayList;
 
