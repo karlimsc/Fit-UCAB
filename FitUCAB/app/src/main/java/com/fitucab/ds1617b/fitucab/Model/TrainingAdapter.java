4747ff1f7f870021ea1588ea818364a7be007517
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-05-24 14:11:18 -0400

    Se agregó los adaptadores para rellenar las vista

---
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/Model/TrainingAdapter.java
@@ -0,0 +1,44 @@
+package com.fitucab.ds1617b.fitucab.Model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.fitucab.ds1617b.fitucab.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alejandro Fernandez on 23/5/2017.
+ */
+
+public class TrainingAdapter extends ArrayAdapter<ArrayAuxiliarTraining>{
+    public TrainingAdapter(Context context, ArrayList<ArrayAuxiliarTraining> items) {
+        super(context , 0, items);
+    }
+
+    @Override
+    public int getViewTypeCount(){
+        return 4;
+    }
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        ArrayAuxiliarTraining item = getItem(position);
+        int type = getItemViewType(position);
+        if (convertView == null) {
+
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_m06_list_adapter, parent, false);
+                TextView textViewNameTraining = (TextView) convertView.findViewById(R.id.m06_nombre_entrenamiento_adapter);
+                textViewNameTraining.setText(item.get_nombreEntrenamiento());
+            }
+
+        return convertView;
+    }
+}
