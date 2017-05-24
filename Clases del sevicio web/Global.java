5e6b6fd29f194e22003183e9d399c19913d14c40
Karolyn <lamitologica@gmail.com>
2017-05-24 11:11:35 -0400

    Eliminadas cosas que no van en el repo.

--- Clases del sevicio web/Global.java
+++
@@ -1,6 +0,0 @@
-public class Global {
-    public static String port = "5432";
-    public static String nameBd ="FitUcabDB";
-    public static String password ="postgres";
-    public static String user="postgres";
-}
