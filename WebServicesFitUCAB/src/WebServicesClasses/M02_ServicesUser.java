9433b09aa3e374ebb3d7002d54422f0181e54fbc
David Garcia <david.a.g.r93@gmail.com>
2017-05-24 15:25:41 -0400

    M02 arreglos en M02_ServicesUser

--- WebServicesFitUCAB/src/WebServicesClasses/M02_ServicesUser.java
+++ WebServicesFitUCAB/src/WebServicesClasses/M02_ServicesUser.java
@@ -15,29 +15,29 @@ public class M02_ServicesUser {
 
     /**
      * Metodo para obtener un usuario
-     * @param username Nombre de usuario que se va a obtener
+     * @param id Id del usuario que se va a obtener
      * @return Clase User en formato json
      * @see User
      */
     @GET
-    @Path("/{username}")
-    public User getUser( @PathParam("username") String username ){
+    @Path("/{userId}")
+    public User getUser( @PathParam("userId") int id ){
         Date date = new Date(1L);
-        return new User( 1, username, "password", "email", "sexo", "4241782944", date );
+        return new User( id, "username", "password", "email", "sexo", "4241782944", date );
     }
 
     /**
      * Metodo para actualizar un usuario
-     * @param username Nombre de usuario que se quiere modificar
+     * @param id Id del usuario que se quiere modificar
      * @param user Datos del usuario modificado
      * @return Clase User en formato json
      * @see User
      */
     @PUT
-    @Path("/{username}")
+    @Path("/{userId}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public User updateUser( @PathParam("username") String username, User user ){
-        user.setUser(username);
+    public User updateUser( @PathParam("userId") int id, User user ){
+        user.setId(id);
         return user;
     }
 
