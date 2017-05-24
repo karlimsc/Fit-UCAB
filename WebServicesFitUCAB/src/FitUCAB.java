c002ac3cd87eb4f57233f162f619f4a8fff5d80b
Daniel Da Silva <daniel_ste@hotmail.com>
2017-05-24 02:16:42 -0400

    Web Service del M04 y arreglos en los script de la bd del M04

--- WebServicesFitUCAB/src/FitUCAB.java
+++ WebServicesFitUCAB/src/FitUCAB.java
@@ -2,7 +2,7 @@
  * Created by root on 14/05/17.
  */
 
-import WebServicesClasses.M01_ServicesUser;
+import WebServicesClasses.*;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -19,6 +19,8 @@ public class FitUCAB extends Application {
     public Set<Class<?>> getClasses() {
         HashSet h= new HashSet<Class<?>>();
         h.add(M01_ServicesUser.class);
+        h.add(M04_ServicesNotificationSettings.class);
         return h;
     }
+
 }
