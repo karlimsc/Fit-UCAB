8bc7a858e4879145bec9c14366fa7a5a66cb1238
Raul Tuozzo <ratuozzo@gmail.com>
2017-05-27 19:08:05 -0400

    location en near me

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M03/M03FragmentNearMe.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M03/M03FragmentNearMe.java
@@ -5,16 +5,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.fitucab.ds1617b.fitucab.Helper.GPSTracker;
 import com.fitucab.ds1617b.fitucab.R;
 
-public class M03FragmentNearMe extends Fragment {
+public class M03FragmentNearMe extends Fragment{
+
+    TextView latitud;
+    TextView longitud;
+
+    View rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_m03_near_me, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_m03_near_me, container, false);
+
+        GPSTracker gps = new GPSTracker(rootView.getContext());
+        if(gps.canGetLocation()){
 
 
+            double dlat = gps.getLatitude();
+            double dlong = gps.getLongitude();
 
+            latitud = (TextView) rootView.findViewById(R.id.nearMeName);
+            longitud = (TextView) rootView.findViewById(R.id.nearMePoints);
+            latitud.setText(String.valueOf(dlat));
+            longitud.setText(String.valueOf(dlong));
+        }
+
+        return rootView;
     }
+
+
 }
