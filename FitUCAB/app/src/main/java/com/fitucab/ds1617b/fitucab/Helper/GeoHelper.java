5be1bea1c0a1529684a79ec7156145086279985c
andres <andresfra92@gmail.com>
2017-05-28 17:09:56 -0400

    Arreglos del merge

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/Helper/GeoHelper.java
+++
@@ -1,8 +0,0 @@
-package com.fitucab.ds1617b.fitucab.Helper;
-
-/**
- * Created by andres on 27/05/17.
- */
-
-public class GeoHelper {
-}
--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/Helper/IpStringConnection.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/Helper/IpStringConnection.java
@@ -8,12 +8,12 @@ package com.fitucab.ds1617b.fitucab.Helper;
  */
 
 public class IpStringConnection {
-    private String _ip = "http://190.74.250.81:8888/WebServicesFitUCAB_war_exploded/";
+    private String _ip = "http://192.168.1.101:8080/WebServicesFitUCAB_war_exploded/";
 
 
     public IpStringConnection() {
 
-        this._ip = "http://190.74.250.81:8888/WebServicesFitUCAB_war_exploded/";
+        this._ip = "http://192.168.1.101:8080/WebServicesFitUCAB_war_exploded/";
 
     }
 
