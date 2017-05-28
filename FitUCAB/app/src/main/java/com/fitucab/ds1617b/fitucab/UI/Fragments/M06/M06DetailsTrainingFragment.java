58742b4d550ac0f5a8987ed7f84cd7aa0ef48882
AlejandroFernandezUCAB <pafernandez.14@est.ucab.edu.ve>
2017-05-27 20:55:17 -0400

    Se arreglaron elementos visuales del Details training y se agregó una lista más grande para llenar los deportes

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M06/M06DetailsTrainingFragment.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M06/M06DetailsTrainingFragment.java
@@ -12,8 +12,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.fitucab.ds1617b.fitucab.Helper.IpStringConnection;
+import com.fitucab.ds1617b.fitucab.Helper.ManagePreferences;
 import com.fitucab.ds1617b.fitucab.Helper.OnFragmentSwap;
+import com.fitucab.ds1617b.fitucab.Model.Training;
 import com.fitucab.ds1617b.fitucab.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alejandro Fernandez on 24/4/2017.
@@ -24,7 +38,6 @@ public class M06DetailsTrainingFragment extends Fragment {
     private View _view;
     private OnFragmentSwap _callBack;
     private ArrayAdapter<String> _adaptador;
-    private String[] entrenamiento = {"Correr,Trotar"};
     private Toolbar _toolbar;
 
 
@@ -47,14 +60,59 @@ public class M06DetailsTrainingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         _view = inflater.inflate(R.layout.fragment_m06_details_training_fragment, container, false);
-        setupViewValues();
+        fillListView();
         return _view;
     }
 
     private void setupViewValues() {
         //Llenando el list View
-        _listView = (ListView) _view.findViewById(R.id.m06_ListViewActividades);
-        _adaptador = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, entrenamiento);
-        _listView.setAdapter(_adaptador);
+
+    }
+
+    public void fillListView(){
+
+        //Url a la cual se va a hacer conexion
+        IpStringConnection ip = new IpStringConnection();
+        String url = ip.getIp() + "M06_ServicesTraining/displayTraining";
+        final Gson gson = new Gson();
+
+        // Instancia RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        _listView = (ListView) _view.findViewById( R.id.m06_listViewEntrenamiento );
+
+        //Se hace la peticion y lo devuelve en String Request
+        StringRequest stringRequest = new StringRequest( Request.Method.GET , url ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ArrayList<String> entrenamiento = new ArrayList<String>();
+                        ArrayList<Training> at = new ArrayList<Training>();
+                        at = gson.fromJson( response , new TypeToken< List< Training > >(){}.getType() );
+                        _entrenamientos = at;
+                        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, entrenamiento);
+                        _listView.setAdapter( adaptador );
+
+
+                        //Hago el for para meter todo en el entrenamiento y asi pasarlo al listview
+                        for(int i = 0;i<at.size();i++){
+                            entrenamiento.add( at.get(i).getTrainingName() );
+                        }
+
+                        adaptador.addAll( entrenamiento );
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ArrayList<String> entrenamiento = new ArrayList<String>();
+                ArrayAdapter<String> adaptador = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, entrenamiento);
+                _listView.setAdapter( adaptador );
+                entrenamiento.add( "Fallo la conexión intente mas tarde");
+                adaptador.addAll( entrenamiento );
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add( stringRequest );
+        _listView.setOnItemClickListener( this );
     }
 }
