551690b4105a8b55e9a17634913251a0a078aa5d
Daniel Rivetti <riverdrakar@gmail.com>
2017-05-29 00:27:51 -0400

    .

--- FitUCAB/app/src/androidTest/java/com/fitucab/ds1617b/fitucab/M01_Test/M01SignUpFragmentTest.java
+++ FitUCAB/app/src/androidTest/java/com/fitucab/ds1617b/fitucab/M01_Test/M01SignUpFragmentTest.java
@@ -1,7 +1,7 @@
 package com.fitucab.ds1617b.fitucab.M01_Test;
 
-import org.junit.After;
-import org.junit.Before;
+import com.fitucab.ds1617b.fitucab.UI.Fragments.M01.M01SignUpFragment;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,38 +10,10 @@ import static org.junit.Assert.*;
  * Created by Daniel on 28-05-2017.
  */
 public class M01SignUpFragmentTest {
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
     @Test
-    public void onAttach() throws Exception {
-
-    }
-
-    @Test
-    public void onCreateView() throws Exception {
-
-    }
-
-    @Test
-    public void instantiateCalendar() throws Exception {
-
-    }
-
-    @Test
-    public void getRetrofit() throws Exception {
-
-    }
-
-    @Test
-    public void onCompleted() throws Exception {
+    public void validateComponents() throws Exception {
+        M01SignUpFragment prueba = new M01SignUpFragment();
+        prueba.getRetrofit("daniel","sdfsdfsdf","daniel@gmail.com","04122584809","12345687sdfsd","11/06/1993","78","1");
 
     }
 
