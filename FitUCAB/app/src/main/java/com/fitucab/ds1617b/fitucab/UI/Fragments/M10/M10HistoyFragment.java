3794cdfa9b48168d4365b838bb5533962205dcbd
Charbel1 <charbelnacharn@gmail.com>
2017-05-30 17:09:03 -0400

    Update
    arreglando archivo

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M10/M10HistoyFragment.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Fragments/M10/M10HistoyFragment.java
@@ -26,6 +26,8 @@ import com.google.gson.reflect.TypeToken;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.fitucab.ds1617b.fitucab.UI.Fragments.M10.M10WaterGlassFragment.user;
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
@@ -41,7 +43,7 @@ public class M10HistoyFragment extends Fragment {
     private static ArrayList<Water>  list = new ArrayList<>();
     private Context contexto;
     private IpStringConnection Url = new IpStringConnection();
-    int idusuario =1;
+    static int idusuario =1;
     M10WaterGlassActivity m10 = new M10WaterGlassActivity();
     static M10WaterGlassFragment m10w;
      Gson gson = new Gson();
@@ -59,6 +61,13 @@ public class M10HistoyFragment extends Fragment {
 
 
         View rootView = inflater.inflate(R.layout.fragment_m10_histoy,container,false);
+        try {
+            idusuario = user.getIdUser(contexto);
+
+        }catch (Exception e)
+        {
+
+        }
         _waterlist = (ListView) rootView.findViewById(R.id.list_water);
         contexto=rootView.getContext();
          adapter = new AdapterM10ListView(rootView.getContext(),FillWater);
