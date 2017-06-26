4ced026c380d7395f331f8c1e27d3041d03c8054
karlimsc <karlianamsuarez@gmail.com>
2017-06-26 13:13:24 -0400

    fabrica de comandos solo comento los que uso

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/CommandsFactory.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/CommandsFactory.java
@@ -3,6 +3,9 @@ package edu.ucab.desarrollo.fitucab.domainLogicLayer;
 import edu.ucab.desarrollo.fitucab.common.entities.Entity;
 import edu.ucab.desarrollo.fitucab.domainLogicLayer.M06.*;
 
+/**
+ * Fabrica de comandos
+ */
 public class CommandsFactory {
 
     static public CreateTrainingCommand instanciateCreateTrainingCmd(Entity training, int userId){
