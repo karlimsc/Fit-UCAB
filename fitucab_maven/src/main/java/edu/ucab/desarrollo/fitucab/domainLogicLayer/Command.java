178c310a5a7f46969f5f5bf186511710d35ce5fb
karlimsc <karlianamsuarez@gmail.com>
2017-06-26 02:03:10 -0400

    patron comando

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/Command.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/Command.java
@@ -1,10 +1,12 @@
 package edu.ucab.desarrollo.fitucab.domainLogicLayer;
 
 
+import edu.ucab.desarrollo.fitucab.common.exceptions.ListAllException;
+import edu.ucab.desarrollo.fitucab.common.exceptions.ListByIdException;
+
 public abstract class Command
 {
 
-
-    public abstract void execute();
+    public abstract void execute() throws ListAllException, ListByIdException;
 
 }
