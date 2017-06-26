3a4c1b4132adc8572b8d59c5368911637477cccb
karlimsc <karlianamsuarez@gmail.com>
2017-06-26 01:52:06 -0400

    clase entrenamiento con comentarios

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/entities/Training.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/entities/Training.java
@@ -1,45 +1,106 @@
 package edu.ucab.desarrollo.fitucab.common.entities;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Clase Entrenamiento M06
+ */
 public class Training extends Entity {
 
-    private int _idTraining;
     private String _trainingName;
     private int _trainingPeriod;
-    private int _trainingCalories;
+    private ArrayList<Entity> _activitylist;
 
-    public Training() {} // Constructor vacio
+    /**
+     * Constructor vacio
+     */
+    public Training() {}
 
-    //Constructor con id
-    public Training(int id, String trainingName, int trainingPeriod, int trainingCalories) {
-        this._idTraining = id;
+    /**
+     * Constructor con id
+     * @param id identificador
+     * @param trainingName nombre del entrenamiento
+     * @param trainingPeriod hora(s) que puede durar la actividad que incluye en el entrenamiento
+     * @param activitylist lista de actividades que tiene el entrenamiento
+     */
+    public Training(int id, String trainingName, int trainingPeriod, LinkedList activitylist) {
+        this._id = id;
         this._trainingName = trainingName;
         this._trainingPeriod = trainingPeriod;
-        this._trainingCalories = trainingCalories;
     }
 
-    //Constructor sin id
-    public Training(String trainingName, int trainingPeriod, int trainingCalories) {
+    /**
+     * Constructor sin id
+     * @param trainingName nombre del entrenamiento
+     * @param trainingPeriod hora(s) que puede durar la actividad que incluye en el entrenamiento
+     * @param activitylist lista de actividades que tiene el entrenamiento
+     */
+    public Training(String trainingName, int trainingPeriod, LinkedList activitylist) {
         this._trainingName = trainingName;
         this._trainingPeriod = trainingPeriod;
-        this._trainingCalories = trainingCalories;
+    }
+
+    /**
+     * Contructor con nombre y periodo
+     * @param _trainingName
+     * @param _trainingPeriod
+     */
+    public Training(String _trainingName, int _trainingPeriod)
+    {
+
+        this._trainingName = _trainingName;
+        this._trainingPeriod = _trainingPeriod;
+    }
+
+    /**
+     * Contructor sin la lista de actividades
+     * @param _id
+     * @param _trainingName
+     * @param _trainingPeriod
+     */
+    public Training(int _id, String _trainingName, int _trainingPeriod)
+    {
+        super(_id);
+        this._trainingName = _trainingName;
+        this._trainingPeriod = _trainingPeriod;
+    }
+    /**
+     * constructor para instanciar solo con el parametro id
+     * @param userId el identificador de base de datos
+     *
+     */
+    public Training(int userId)
+    {
+        set_id( userId );
+    }
+
+    /**
+     * Contructor con id y nombre
+     * @param id
+     * @param name
+     */
+    public Training( int id, String name )
+    {
+        set_id( id );
+
+        setTrainingName( name );
+
     }
 
     //Getters and setters basicos
-
-    public int getidTraining() {
-        return _idTraining;
-    }
-
-    public void setId(int id) {
-        this._idTraining = id;
-    }
 
     public String getTrainingName() {
         return _trainingName;
     }
 
-    public void setTrainingName(String trainingName) {
-        this._trainingName = trainingName;
+    public void setTrainingName(String trainingName)
+    {
+        if ( trainingName.equals( "" ) )
+            new InvalidParameterException( "mensaje" );
+
+        _trainingName = trainingName;
     }
 
     public int getTrainingPeriod() {
@@ -50,12 +111,13 @@ public class Training extends Entity {
         this._trainingPeriod = trainingPeriod;
     }
 
-    public int getTrainingCalories() {
-        return _trainingCalories;
+    public ArrayList<Entity> get_activitylist()
+    {
+        return _activitylist;
     }
 
-    public void setTrainingCalories(int trainingCalories) {
-        this._trainingCalories = trainingCalories;
+    public void set_activitylist(ArrayList<Entity> _activitylist)
+    {
+        this._activitylist = _activitylist;
     }
-
 }
