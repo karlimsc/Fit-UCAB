30b20921db694c34e1e45ee9429cd137a952f054
karlimsc <karlianamsuarez@gmail.com>
2017-06-26 01:48:49 -0400

    clase Entidad

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/entities/Entity.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/entities/Entity.java
@@ -1,9 +1,24 @@
 package edu.ucab.desarrollo.fitucab.common.entities;
 
-
+/**
+ *
+ */
 public class Entity {
+
+        public int _id;
+
+        public int get_id() {
+                return _id;
+        }
+
+        public void set_id(int _id) {
+                this._id = _id;
+        }
 
         public Entity (){}
 
+        public Entity(int _id){
+                this._id=_id;
+        }
 
 }
