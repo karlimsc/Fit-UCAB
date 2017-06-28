9a26cc96a88ef19016a55f0a143810fff64e6bac
Jessica Elberg <elbergjessica@gmail.com>
2017-06-28 02:12:07 -0400

    Pasando a Limprio M01_ServicesUser1

---
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/webService/M01_ServicesUser1.java
@@ -0,0 +1,40 @@
+package edu.ucab.desarrollo.fitucab.webService;
+
+/**
+ * Created by elberg on 28/06/17.
+ */
+
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.sql.Connection;
+
+import static edu.ucab.desarrollo.fitucab.webService.Sql.getConInstance;
+
+/**
+ * Clase de Servicios Web del Modulo 01
+ */
+@Path("/M01_ServicesUser")
+public class M01_ServicesUser1 {
+    Gson gson = new Gson();
+    private Connection conn = getConInstance();
+
+    public M01_ServicesUser1() {}
+
+    /**
+     * Metodo que es llamado a traves del web service para consultar un usuario
+     * existente en la base de datos
+     * @return el usuario con los datos que trae la consulta
+     */
+    @GET
+    @Path("/login_user")
+    @Produces("application/json")
+    public String getUser(@QueryParam("username") String username,
+                          @QueryParam("password") String password)
+    {
+        return null ;
+    }
+}
