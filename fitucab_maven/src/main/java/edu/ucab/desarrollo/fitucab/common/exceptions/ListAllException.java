aa249f513816fbc58821434cde33a710301783f0
karlimsc <karlianamsuarez@gmail.com>
2017-06-28 12:20:57 -0400

    excepcion para todos

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/exceptions/ListAllException.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/exceptions/ListAllException.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
  */
 public class ListAllException extends Exception
 {
+    public final int ERROR_CODE = 552;
+    public final String ERROR_MSG = "Ha ocurrido un error consultado todas las entidades";
+
     /**
      * Excepcion personalizada para listar todos
      * @param e
@@ -14,5 +17,19 @@ public class ListAllException extends Exception
     public ListAllException(SQLException e)
     {
         super( e );
+    }
+
+
+    @Override
+    public String toString()
+    {
+        StringBuilder str = null;
+
+
+        str = new StringBuilder( ERROR_CODE + "\n" );
+        str.append( ERROR_MSG + "\n" );
+        str.append( super.toString() );
+
+        return str.toString();
     }
 }
