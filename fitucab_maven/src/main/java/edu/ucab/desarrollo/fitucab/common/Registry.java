d7aab883f1b816af62f6560ec09b6a61bfeec451
karlimsc <karlianamsuarez@gmail.com>
2017-06-28 11:50:56 -0400

    Registro modificado para todos

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/Registry.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/Registry.java
@@ -11,4 +11,11 @@ public class Registry
     public static String BD_URL = "jdbc:postgresql://localhost/fitucabdb";
     public static String BD_CLASS_FOR_NAME = "org.postgresql.Driver";
 
+
+    public static final int RESULT_CODE_OK = 200;
+    public static final int RESULT_CODE_FAIL = 500;
+
+    public static final String ERROR_PARAM_WS = "Error en el parametro {} recibido por el servicio web";
+    public static final String RESULT_CODE_FAIL_MSG = "Ha ocurrido un error generico";
+
 }
