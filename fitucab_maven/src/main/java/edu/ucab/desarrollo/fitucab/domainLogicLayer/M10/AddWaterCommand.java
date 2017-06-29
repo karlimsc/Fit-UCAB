18a9f39093b01e6b885e0696c46b0104c1952bbb
Raul Tuozzo <ratuozzo@gmail.com>
2017-06-29 01:28:33 -0400

    prueba DAOS

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/M10/AddWaterCommand.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/domainLogicLayer/M10/AddWaterCommand.java
@@ -2,7 +2,14 @@ package edu.ucab.desarrollo.fitucab.domainLogicLayer.M10;
 
 import edu.ucab.desarrollo.fitucab.common.entities.Entity;
 
+import edu.ucab.desarrollo.fitucab.common.entities.Water;
+import edu.ucab.desarrollo.fitucab.common.exceptions.AddException;
+import edu.ucab.desarrollo.fitucab.dataAccessLayer.DaoFactory;
+import edu.ucab.desarrollo.fitucab.dataAccessLayer.M10.IDaoWater;
 import edu.ucab.desarrollo.fitucab.domainLogicLayer.Command;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Comando para agregar agua que extiende de command
@@ -20,6 +27,14 @@ public class AddWaterCommand extends Command {
     public AddWaterCommand (Entity water){ _water = water; };
 
     public void execute() {
-        returned = "HOLA BEBE";
+
+        IDaoWater daoWater = DaoFactory.instanceDaoWater(_water);
+        try {
+            Water water = (Water) daoWater.addWater(_water);
+            returned = water.get_fkPerson().toString();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //returned = "HOLA BEBE";
     }
 }
