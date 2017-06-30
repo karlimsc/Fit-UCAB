8c801b3826a7b523d9c3af4ff9d09ad72d2a8ba7
bdc884 <bdc884@gmail.com>
2017-06-29 21:22:31 -0400

    Exceptions Web Service

---
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/exceptions/M02/CreateHomeException.java
@@ -0,0 +1,33 @@
+package edu.ucab.desarrollo.fitucab.common.exceptions.M02;
+
+/**
+ * Excepcion personalizada del Modulo 2 para el comando Create Home.
+ * @author Juan Macedo, Cesar Boza, Bryan Teixeira
+ * @version 1.0
+ */
+public class CreateHomeException extends Exception{
+
+    private int _code = 551;
+    private String _class;
+    private String _method;
+    private String _specificException;
+
+    public CreateHomeException(Exception error, String _class, String _method, String _specificException) {
+        super(error);
+        _class = _class;
+        _method =_method;
+        _specificException=_specificException;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder _strB = new StringBuilder(_code);
+        _strB.append(_class);
+        _strB.append(_method);
+        _strB.append(_specificException);
+        _strB.append(super.toString());
+        return super.toString();
+    }
+
+}
