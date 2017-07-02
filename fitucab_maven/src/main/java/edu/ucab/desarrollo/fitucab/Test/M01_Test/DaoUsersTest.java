538a9bdb428cdfb554ab2ea62bb4cc09751ad164
Jessica Elberg <elbergjessica@gmail.com>
2017-07-02 15:04:36 -0400

    Arreglado la PU del create

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M01_Test/DaoUsersTest.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M01_Test/DaoUsersTest.java
@@ -1,10 +1,15 @@
 //package edu.ucab.desarrollo.fitucab.Test.M01_Test;
 
 import edu.ucab.desarrollo.fitucab.common.entities.Entity;
+import edu.ucab.desarrollo.fitucab.common.entities.EntityFactory;
 import edu.ucab.desarrollo.fitucab.common.entities.User;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.Dao;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.DaoFactory;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.M01.DaoUser;
+import edu.ucab.desarrollo.fitucab.dataAccessLayer.Security;
+import edu.ucab.desarrollo.fitucab.domainLogicLayer.Command;
+import edu.ucab.desarrollo.fitucab.domainLogicLayer.CommandsFactory;
+import edu.ucab.desarrollo.fitucab.domainLogicLayer.M01.CreateUserCommand;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -36,20 +41,6 @@ public class DaoUsersTest {
 
 
 
-    public void limpiar() throws Exception {
-        try{
-
-            CallableStatement cs = conn.prepareCall("{=call M01_ELIMINARUSER(?)}");
-            cs.setString(1, "naomi");
-            cs.execute();
-            conn.close();
-
-        }
-        catch (Exception e){
-
-        }
-    }
-
     @Test
     @Ignore
     public void login() throws Exception {
@@ -58,32 +49,44 @@ public class DaoUsersTest {
     @Test
     public void create() throws Exception {
         conn = _dao.getConInstance();
-        _user = new User(60,"naomi","123",
+        _user = EntityFactory.createUser(60,"naomi","123",
                 "@gmail","f","00000",
                 Date.valueOf("2010-12-12"),
                 12,12);
 
-        DaoUser login = (DaoUser) DaoFactory.instanciateDaoUser(_user);
-        login.login(_user);
+        Command _command = CommandsFactory.instanciateCreateUserCmd(_user);
+        CreateUserCommand cmd = (CreateUserCommand) _command;
+        cmd.execute();
+
 
         //Se trae el ultimo usuario registrado
-        CallableStatement cstmt =conn.prepareCall("{?=call M01_LASTUSER()}");
+       CallableStatement cstmt =conn.prepareCall("{?=call M01_LASTUSER()}");
         cstmt.registerOutParameter(1, Types.INTEGER);
         cstmt.execute();
 
         int id_insert = cstmt.getInt(1);
-        System.out.printf("ID registro = "+id_insert);
+
 
         //Busca el usuario que fue registrado
+
+        Security _sc = new Security();
+        String password =_sc.encryptPassword(String.valueOf(123));
+
         CallableStatement cs = conn.prepareCall("{?=call M01_INICIARSESION(?,?)}");
         cs.registerOutParameter(1, Types.INTEGER);
         cs.setString(2, "naomi");
-        cs.setString(3, "123");
+        cs.setString(3, password);
         cs.execute();
 
         int id_login = cs.getInt(1);
-        System.out.printf("ID Login = "+id_login);
-        limpiar();
+
+        //Elimina la insercion
+        CallableStatement cst = conn.prepareCall("{?=call M01_ELIMINARUSER(?)}");
+        cst.registerOutParameter(1, Types.INTEGER);
+        cst.setString(2, "naomi");
+        cst.execute();
+        conn.close();
+
         assertEquals(id_insert,id_login);
     }
 
