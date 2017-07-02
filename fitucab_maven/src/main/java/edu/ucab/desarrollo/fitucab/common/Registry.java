9f4631b3acd4372e5f8e171ab2260a1dda055abe
Charbel <charbelnacharn@gmail.com>
2017-07-02 18:02:15 -0400

    Merge branch 'Develop' into M11_Entrega2

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/Registry.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/common/Registry.java
@@ -5,10 +5,9 @@ package edu.ucab.desarrollo.fitucab.common;
  */
 public class Registry
 {
-
-    public static String BD_USER = "postgres";
-    public static String BD_PASSWORD = "postgres";
-    public static String BD_URL = "jdbc:postgresql://localhost/fitucabdb";
+    public static String BD_USER = "fitucab";
+    public static String BD_PASSWORD = "fitucab";
+    public static String BD_URL = "jdbc:postgresql://localhost/FitUcabDB";
     public static String BD_CLASS_FOR_NAME = "org.postgresql.Driver";
     public static String RECOVERY_EMAIL_USERNAME = "fitucabprueba2@gmail.com";
     public static String RECOVERY_EMAIL_PASS = "ucab2017";
