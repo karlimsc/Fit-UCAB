02fba6938443e9ae3491f45d5a54b5a460c09d22
Charbel <charbelnacharn@gmail.com>
2017-07-02 10:48:07 -0400

    moment ws

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M11/DaoMoment.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M11/DaoMoment.java
@@ -1,10 +1,16 @@
 package edu.ucab.desarrollo.fitucab.dataAccessLayer.M11;
 
+import com.google.gson.Gson;
 import edu.ucab.desarrollo.fitucab.common.entities.Entity;
 import edu.ucab.desarrollo.fitucab.common.entities.Moment;
 import edu.ucab.desarrollo.fitucab.common.exceptions.AddException;
+import edu.ucab.desarrollo.fitucab.common.exceptions.BdConnectException;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.Dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -13,6 +19,8 @@ import java.util.ArrayList;
 public class DaoMoment extends Dao implements iDaoMoment {
 
     ArrayList<Moment> jsonArray;
+    Gson gson = new Gson();
+
     @Override
     public void Create(Entity e) {
 
@@ -24,10 +32,24 @@ public class DaoMoment extends Dao implements iDaoMoment {
     }
 
     @Override
-    public Entity read(Entity e) {
+    public Entity read(Entity e) throws SQLException, BdConnectException {
 
+        String query = "Select * from m11_get_momentos()";
+        jsonArray = new ArrayList<>();
+        Moment moment = (Moment) e;
 
-        return e;
+            Connection conn = Dao.getBdConnect();
+            PreparedStatement st = conn.prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                jsonArray.add(new Moment());
+                jsonArray.get(rs.getRow() - 1).set_description(rs.getString("momento"));
+                jsonArray.get(rs.getRow() - 1).set_id(rs.getInt("momento_id"));
+            }
+            moment.jsonArray = jsonArray;
+
+            return moment;
+
     }
 
     @Override
