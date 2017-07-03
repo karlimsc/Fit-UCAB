e362b0f28f742fa55e20bd8bccaea71849475ecb
Daniel Da Silva <daniel_ste@hotmail.com>
2017-07-03 17:35:44 -0400

    nueva pu y arreglos

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M03_FriendshipTest/FriendshipTest.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M03_FriendshipTest/FriendshipTest.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import edu.ucab.desarrollo.fitucab.webService.M03_ServicesContacts;
+import edu.ucab.desarrollo.fitucab.webService.Sql;
 import edu.ucab.desarrollo.fitucab.common.entities.Entity;
 import edu.ucab.desarrollo.fitucab.common.entities.EntityFactory;
 import edu.ucab.desarrollo.fitucab.common.entities.Friendship;
@@ -11,7 +13,12 @@ import edu.ucab.desarrollo.fitucab.dataAccessLayer.Dao;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.DaoFactory;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.M03.IDaoFriendship;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.M03.IDaoNearMe;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 
@@ -22,6 +29,30 @@ public class FriendshipTest {
     static Connection conn;
     static Dao _dao;
     static Entity _user;
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        Sql basePruebaFriendTearDown = new Sql();
+
+        //Eliminamos los dos usuarios de prueba.
+        String query = "DELETE from person WHERE (personid= 998) or (personid = 999) ";
+
+        basePruebaFriendTearDown.sqlNoReturn(query);
+
+        basePruebaFriendTearDown = new Sql();
+
+        //Eliminamos los dos usuarios de prueba.
+        query = "DELETE from friendship WHERE (fk_persononeid = 998) and (fk_persontwoid = 999) ";
+
+        basePruebaFriendTearDown.sqlNoReturn(query);
+    }
 
     /**
      * Prueba unitaria para comprobar que el array usado en el metodo getList no sea nulo
@@ -81,6 +112,50 @@ public class FriendshipTest {
         IDaoFriendship daofriend = DaoFactory.instanceDaoFriendship(_friend);
         int x = daofriend.mayor("1","2");
         assertEquals(2,x);
+
+    }
+
+    @Test
+    public void getContacts() throws Exception {
+
+        Gson gson = new Gson();
+        Sql basePruebaFriendSetup = new Sql();
+
+        String query = "insert into person (personid,personusername, personpassword, personemail, personsex," +
+                " personphone, personbirthdate) values (998,'UsuarioPrueba', 'Password', 'email@about.com', 'F'," +
+                " '04156665488', '31/7/2016'),(999,'UsuarioPrueba2', 'Password', 'email@about.com2', 'F'," +
+                " '04144458587', '31/7/2016');";
+
+        basePruebaFriendSetup.sqlNoReturn(query);
+
+        ArrayList<UserAuxiliar> libreta = new ArrayList<UserAuxiliar>();
+        libreta.add(new UserAuxiliar("pepe","email.com","22222"));
+        libreta.add(new UserAuxiliar("UsuarioPrueba2","email@about.com2","04144458587"));
+
+
+        M03_ServicesContacts m3Contacts = new M03_ServicesContacts();
+        String entrada = m3Contacts.getContacts("998",gson.toJson(libreta));
+
+        ArrayList<UserAuxiliar> salida = new ArrayList<UserAuxiliar>();
+
+        salida = gson.fromJson(entrada,new TypeToken<List<UserAuxiliar>>(){}.getType());
+
+        ArrayList<UserAuxiliar> compare = new ArrayList<UserAuxiliar>();
+
+
+        UserAuxiliar sep1 = new UserAuxiliar();
+        sep1.set_id(-2);
+
+        compare.add(sep1);
+        compare.add(new UserAuxiliar(0,"UsuarioPrueba2",0,0));
+
+        assertEquals(salida.get(0).get_id(),compare.get(0).get_id());
+        assertEquals(salida.get(1).get_id(), compare.get(1).get_id());
+
+
+
+
+
 
     }
 
