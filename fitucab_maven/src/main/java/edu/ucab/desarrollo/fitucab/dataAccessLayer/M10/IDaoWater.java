925232d24dcc3b4f924c24eb98bb78586e851a1d
JoseA2R <joseagustindrf@gmail.com>
2017-07-03 02:22:24 -0400

    arreglando conflictos

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M10/IDaoWater.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M10/IDaoWater.java
@@ -7,17 +7,14 @@ import edu.ucab.desarrollo.fitucab.dataAccessLayer.IDao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-/**
- * Created by Raul A on 6/28/2017.
- */
+
 public interface IDaoWater extends IDao{
 
 
     public Entity create(Entity e) throws SQLException;
     public ArrayList<Water> getList (Entity e)  throws  SQLException;
     public Entity getWater(Entity e)  throws  SQLException;
-    public Entity getFechaInt(Entity e)  throws  SQLException;
     public Entity deleteLast(Entity e)  throws  SQLException;
-    public Entity deleteWaterTm(Entity e)  throws  SQLException;
+
 
 }
