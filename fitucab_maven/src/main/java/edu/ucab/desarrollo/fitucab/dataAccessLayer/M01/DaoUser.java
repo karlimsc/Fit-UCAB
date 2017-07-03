c51648014b690cf9585bc51647161e47caca611f
Karolyn <lamitologica@gmail.com>
2017-07-03 19:24:15 -0400

    SSL en testemail.

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M01/DaoUser.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M01/DaoUser.java
@@ -419,6 +419,7 @@ public class DaoUser extends Dao implements IDaoUser {
             Properties props = new Properties();
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
             props.put("mail.smtp.host", "smtp.gmail.com");
             props.put("mail.smtp.port", "587");
 
