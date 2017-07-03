be64916568f96b085a88105ba61f3d9523abceb9
karlimsc <karlianamsuarez@gmail.com>
2017-07-02 23:48:17 -0400

    Merge branch 'M06_Entrega2' of https://github.com/karlimsc/Fit-UCAB into M06_Entrega2
    Conflicts:
    	fitucab_maven/pom.xml
    	fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/dataAccessLayer/M06/DaoTraining.java
    	fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/webService/M06_ServicesTraining.java

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M06/DaoTrainingTest.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M06/DaoTrainingTest.java
@@ -1,9 +1,14 @@
-package M06;
+package edu.ucab.desarrollo.fitucab.Test.M06;
 
+import edu.ucab.desarrollo.fitucab.common.entities.Entity;
+import edu.ucab.desarrollo.fitucab.common.entities.Training;
+import edu.ucab.desarrollo.fitucab.common.entities.User;
 import edu.ucab.desarrollo.fitucab.common.exceptions.ListAllException;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.DaoFactory;
 import edu.ucab.desarrollo.fitucab.dataAccessLayer.M06.DaoTraining;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -68,5 +73,38 @@ class DaoTrainingTest {
     @Test
     void removeActivities() {
     }
+    @Test
+    public void testListAll() throws Exception
+    {
+        User usuario = new User();
+        Training entrenamiento = new Training();
+        DaoTraining dao = new DaoTraining(entrenamiento);
+        LinkedList<Entity> lista;
 
+        usuario.set_id( 1 );
+
+        lista = dao.listAll( usuario );
+
+        assertFalse( lista.isEmpty() );
+
+    }
+
+
+
+
+    @Test
+    public void testTrainingDetail() throws Exception
+    {
+        Training entrenamiento = new Training();
+        DaoTraining dao = new DaoTraining(entrenamiento);
+        Entity resultado;
+
+        entrenamiento.set_id( 1 );
+
+        resultado = ( Training ) dao.trainingDetail( entrenamiento );
+
+        assertEquals( 1, resultado.get_id() );
+        assertEquals( "default", ( ( Training )resultado ).getTrainingName() );
+
+    }
 }
