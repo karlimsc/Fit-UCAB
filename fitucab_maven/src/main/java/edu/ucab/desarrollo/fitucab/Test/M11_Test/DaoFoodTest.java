a590dbe5a8e1e4d5e432c0deaa5c42c093132e1a
Charbel <charbelnacharn@gmail.com>
2017-07-03 20:26:18 -0400

    pruebas

--- fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M11_Test/DaoFoodTest.java
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M11_Test/DaoFoodTest.java
@@ -1,7 +1,44 @@
 package edu.ucab.desarrollo.fitucab.Test.M11_Test;
 
+import edu.ucab.desarrollo.fitucab.common.entities.EntityFactory;
+import edu.ucab.desarrollo.fitucab.common.entities.Moment;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 /**
  * Created by charbel on 03/07/2017.
  */
 public class DaoFoodTest {
+
+
+
+
+
+    @Test
+    public void Momentv ()
+    {
+        Moment a =(Moment) EntityFactory.getMoments();
+        assertNotNull(a);
+
+    }
+
+    @Test
+    public void Momentinstr()
+    {
+        Moment a = (Moment) EntityFactory.getMoments();
+        a.set_id(1);
+        assertEquals(a.get_id(),1);
+
+    }
+
+    @Test
+    public void set_des()
+    {
+        Moment a = (Moment) EntityFactory.getMoments();
+        a.set_description("hola");
+        assertEquals(a.get_description(),"hola");
+
+    }
 }
---
+++ fitucab_maven/src/main/java/edu/ucab/desarrollo/fitucab/Test/M11_Test/MomentTest.java
@@ -0,0 +1,12 @@
+package edu.ucab.desarrollo.fitucab.Test.M11_Test;
+
+import edu.ucab.desarrollo.fitucab.common.entities.Water;
+
+public class MomentTest {
+
+  Water water1 = new Water(4,2);
+  Water water2 = new Water(1,5,"02/10/3000");
+  Water water3 = new Water("error");
+
+
+}
