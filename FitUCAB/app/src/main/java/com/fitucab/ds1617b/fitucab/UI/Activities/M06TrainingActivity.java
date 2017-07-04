8dd38d761351eacfd5f0bd5e2e7ab26ae82f89f8
karlimsc <karlianamsuarez@gmail.com>
2017-07-03 23:13:59 -0400

    arreglo rapido

--- FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M06TrainingActivity.java
+++ FitUCAB/app/src/main/java/com/fitucab/ds1617b/fitucab/UI/Activities/M06TrainingActivity.java
@@ -19,12 +19,7 @@ import com.fitucab.ds1617b.fitucab.R;
 import com.fitucab.ds1617b.fitucab.UI.Fragments.M06.M06AddTrainingFragment;
 import com.fitucab.ds1617b.fitucab.UI.Fragments.M06.M06DetailsTrainingFragment;
 import com.fitucab.ds1617b.fitucab.UI.Fragments.M06.M06HomeTrainingFragment;
-<<<<<<< HEAD
-import com.fitucab.ds1617b.fitucab.UI.Fragments.M06.M06UpdateTraining;
-=======
 import com.fitucab.ds1617b.fitucab.UI.Fragments.M06.M06UpdateTrainingFragment;
->>>>>>> 21507c5dd54ba1e2e88803c81c5882e998ba596a
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -85,15 +80,6 @@ public class M06TrainingActivity extends AppCompatActivity implements OnFragment
                     fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
                     fragmentTransaction.addToBackStack("M06HomeTrainingFragment");
                     break;
-
-                case "M06UpdateTraining":
-                    fragmentToSwap = new M06UpdateTraining();
-                    fragmentToSwap.setArguments(bundle);
-                    fragmentTransaction.replace(R.id.flContent, fragmentToSwap);
-                    fragmentTransaction.addToBackStack("M06HomeTrainingFragment");
-                    break;
-
-
                 case "M06UpdateTrainingFragment":
                     fragmentToSwap = new M06UpdateTrainingFragment();
                     fragmentToSwap.setArguments(bundle);
